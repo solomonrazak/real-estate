2ef4409d5e45c62b2f264be145017668e29f7903
solomonrazak <solomonrazak99@gmail.com>
2024-09-09 09:08:54 -0700

    mobile homepage

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -23,12 +23,12 @@ const SearchBar: React.FC = () => {
         <button className={`py-1 px-4 rounded-tl-sm ${activeButton === "buy" ? "bg-black border border-black text-white" : "bg-gray-100 border border-gray-400 text-gray-700"}`} onClick={handleBuyClick}>Buy</button>
         <button className={`py-1 px-4 rounded-tr-sm ${activeButton === "rent" ? "bg-black border border-black text-white" : "bg-gray-100 text-gray-700 border border-gray-400"}`} onClick={handleRentClick}>Rent</button>
       </div>
-      <div className="md:border border-gray-400 md:mr-4">
-        <form className="flex flex-col md:flex-row">
-          <input type="text" name="location" placeholder="City Location" className="w-full p-2 outline-none border border-gray-400"/>
-          <input type="number" min={0} max={100000} name="minPrice" placeholder="Min Price" className="w-full p-2 outline-none"/>
-          <input type="number" min={0} max={100000} name="maxPrice" placeholder="Max Price" className="w-full pl-2 pr-4 outline-none"/>
-          <button className="bg-yellow-500 px-4"><FaSearch className="text-white"/></button>
+      <div className="mt-2 md:mt-0 md:border border-gray-400 md:mr-4">
+        <form className="flex flex-col gap-2 md:gap-0 md:flex-row">
+          <input type="text" name="location" placeholder="City Location" className="w-full p-2 outline-none border md:border-0 border-gray-400"/>
+          <input type="number" min={0} max={100000} name="minPrice" placeholder="Min Price" className="w-full p-2 outline-none border md:border-0 border-gray-400"/>
+          <input type="number" min={0} max={100000} name="maxPrice" placeholder="Max Price" className="w-full py-2 pl-2 pr-4 outline-none border md:border-0 border-gray-400"/>
+          <button className="bg-yellow-500 px-4 justify-center p-3 flex"><FaSearch className="text-white"/></button>
         </form>
       </div>
 
