c7a50c6d046950ebc9ccbb876b3d99d316de8e6b
solomonrazak <solomonrazak99@gmail.com>
2024-11-18 18:30:01 -0800

    register update

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import bg from "../../assets/images/bg.png";
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Button";
@@ -8,8 +8,8 @@ const Register: React.FC = () => {
   const [error, setError] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const form = e.target as HTMLFormElement
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
     const username = formData.get("username");
     const email = formData.get("email");
@@ -17,24 +17,27 @@ const Register: React.FC = () => {
 
     console.log(username, email, password);
     try {
-    const res = await axios.post('http://localhost:8005/api/auth/register', {username, email, password});
-    console.log(res.data);
+      const res = await axios.post("http://localhost:8005/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-console.log(error)
-    }
-  }
+  };
   return (
     <div className="flex h-full overflow-y-hidden">
       <div className="w-full md:w-[60%] flex flex-col items-center pt-[50%] md:pt-[5rem] lg:pt-[8rem] xl:pt-[13rem] overflow-y-hidden">
         <p className="mb-7 font-bold text-[20px]">Create an Account</p>
-        
+
         <form className="flex flex-col space-y-5" onSubmit={handleSubmit}>
           <Input name="username" type="text" placeholder="Username" />
           <Input name="email" type="email" placeholder="Email" />
           <Input name="password" type="password" placeholder="Password" />
-          <Button name="Register" type="submit"/>
-          
+          <Button name="Register" type="submit" />
+
           <p className="underline">Do you have an acccount?</p>
         </form>
       </div>
