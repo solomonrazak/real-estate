39c1092a7e90c09703d5169b01affb37184522cc
solomonrazak <solomonrazak99@gmail.com>
2024-12-03 16:53:41 -0800

    context update

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,7 +1,37 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, ReactNode } from "react";
 
 
-type AuthContextData {
-    
+type AuthContextData = {
+    currentUser: string;
+    setCurrentUser: React.Dispatch<React.SetStateAction<string>>
+
 }
-export const authContext = createContext()
+
+export const authContext = createContext<AuthContextData | undefined>(undefined);
+
+interface AuthContextProviderProps {
+    children: ReactNode;
+}
+
+// provider
+export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({children}) => {
+  const [currentUser, setCurrentUser] = useState<string>("");
+
+  return (
+    <authContext.Provider value={{currentUser, setCurrentUser}}>
+        {children}
+    </authContext.Provider>
+  )
+
+};
+
+// consumer 
+
+export const useAuth = () => {
+    const context = useContext(authContext);
+    if(!context) {
+        throw new Error ("useAuth must be within a provider")
+    }
+    return context;
+}
+
